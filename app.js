68bd3e339b06621b7eec77d748f66aa6c0e6d9e0
AdityaS1203 <87704157+AdityaS1203@users.noreply.github.com>
2022-10-30 15:06:55 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,44 @@
+const express=require("express");
+const app=express();
+const https=require("https");
+const bodyParser=require("body-parser");
+
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static(__dirname+"/public"));
+
+app.get("/",function(req,res){
+    res.sendFile(__dirname+"/index.html");
+    });
+
+
+app.post("/",function(req,res){
+  var c=req.body.city;
+  const apiKey="62263460b345f14c7234e63b38b23e9b";
+  const cityUrl="https://api.openweathermap.org/geo/1.0/direct?q="+c+"&appid="+apiKey;
+  https.get(cityUrl,function(response){
+    response.on("data",function(data){
+      const weatherData=JSON.parse(data);
+         var lat=weatherData[0].lat;
+         var lon=weatherData[0].lon;
+         const urlfortemp="https://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&units=metric&appid="+apiKey;
+         https.get(urlfortemp,function(responses){
+         responses.on("data",function(data){
+           const values=JSON.parse(data);
+           var temperature=values.main.temp;
+           const icon=values.weather[0].icon;
+           const description=values.weather[0].description;
+           const imgURL="http://openweathermap.org/img/wn/"+ icon +"@2x.png";
+           res.write("<p>Current weather is "+description+"</p>");
+           res.write("<h1>Temperature in "+c+" is "+temperature+" (in celcius)</h1>");
+           res.write("<img src="+ imgURL + ">");
+           res.send();
+         });
+         });
+  });
+  });
+});
+
+app.listen(process.env.PORT||3000,function(){
+  console.log("server for weather app is running");
+});
---
+++ public/js/index.js
@@ -0,0 +1,2 @@
+document.querySelector('.div1').style.backgroundImage="url('images/weather1.jpg')";
+document.querySelector('.div1').style.backgroundSize="cover";
